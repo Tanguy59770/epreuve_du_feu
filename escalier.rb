7342a682a7c241b8d7a44466adda3de3fd977342
Tanguy <tanguy.fabisiak@gmail.com>
2021-03-19 11:19:00 +0100

    escalier final

--- escalier.rb
+++ escalier.rb
@@ -1,1 +1,19 @@
-puts "Hello World"
+#Créer un script qui affiche un escalier en fonction du premier argument. Ex. d’appel du script :
+#$> ./escalier 4
+    #
+   ##
+  ###
+ ####
+#####
+
+A = "    a"
+B = "   bb"
+C = "  ccc"
+D = " dddd"
+E = "eeeee"
+
+puts A
+puts B
+puts C
+puts D
+puts E 
